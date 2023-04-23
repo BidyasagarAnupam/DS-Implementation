ea90e76a0cb3a791d3fb856487060285936e2d39
Bidyasagar Mohapatra <bidyasagarofficial123@gmail.com>
2023-04-23 22:47:08 +0530

    DeQue changed

--- Queue/3_deque.cpp
+++ Queue/3_deque.cpp
@@ -1,130 +1,145 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-const int size = 5;
-
-struct deQue {
-    int arr[size];
-    int front;
-    int rare;
+const int Size = 5;
+struct deque {
+    int arr[Size];
+    int front, rear;
 };
-
-void insert_rare_end(deQue *dq) {
-    int item;
-    if (dq->front == 0 && dq->rare == size - 1 || (dq->front == dq->rare + 1)) {
-        cout << "Deque is overflow\n";
+void insertion_at_front(deque *dq) {
+    int x;
+    if ((dq->front == 0 && dq->rear == Size - 1) ||
+        (dq->rear + 1 == dq->front)) {
+        cout << "Queue is Full\n";
     } else {
         if (dq->front == -1) {
+            dq->rear = 0;
             dq->front = 0;
-            dq->rare = 0;
-        } else {
-            dq->rare = (dq->rare + 1) % size;
-        }
-        cout << "Enter the element to be inserted: ";
-        cin >> item;
-        dq->arr[dq->rare] = item;
-    }
-}
-
-void insert_front_end(deQue *dq) {
-    int item;
-    if (dq->front == 0 && dq->rare == size - 1 || (dq->front == dq->rare + 1)) {
-        cout << "Deque is overflow\n";
-    } else {
-        if (dq->front == -1) {
-            dq->front = 0;
-            dq->rare = 0;
         } else {
             if (dq->front == 0) {
-                dq->front = size - 1;
+                dq->front = Size - 1;
             } else {
-                dq->front = (dq->front - 1);
+                dq->front = dq->front - 1;
             }
         }
-        cout << "Enter the element to be inserted: ";
-        cin >> item;
-        dq->arr[dq->front] = item;
+        cout << "Enter the Element to be Inserted: ";
+        cin >> x;
+        dq->arr[dq->front] = x;
+        cout << x << " is successfully Inserted\n";
     }
 }
 
-void delete_rare_end(deQue *dq) {
-    if (dq->front == -1) {
-        cout << "Deque is empty\n";
+void insertion_at_rear(deque *dq) {
+    int x;
+    if ((dq->front == 0 && dq->rear == Size - 1) ||
+        (dq->front == dq->rear + 1)) {
+        cout << "Queue is Full\n";
     } else {
-        cout << "Element " << dq->arr[dq->rare] << " is deleted\n";
-        if (dq->rare == dq->front) {
-            dq->front = -1;
-            dq->rare = -1;
+        if (dq->front == -1) {
+            dq->rear = 0;
+            dq->front = 0;
         } else {
-            dq->rare = (dq->rare - 1) % size;
+            dq->rear = (dq->rear + 1) % Size;
         }
+        cout << "Enter the Element to be Inserted: ";
+        cin >> x;
+        dq->arr[dq->rear] = x;
+        cout << x << " is successfully Inserted\n";
     }
 }
 
-void delete_front_end(deQue *dq) {
-    if (dq->front == -1) {
-        cout << "Deque is empty\n";
-    } else {
-        cout << "Element " << dq->arr[dq->front] << " is deleted\n";
-        if (dq->rare == dq->front) {
+void deletion_from_front(deque *dq) {
+    int x;
+    if (dq->front == -1)
+        cout << "Queue is Empty\n";
+    else {
+        x = dq->arr[dq->front];
+        if (dq->front == dq->rear) {
             dq->front = -1;
-            dq->rare = -1;
+            dq->rear = -1;
         } else {
-            dq->front = (dq->front + 1) % size;
+            dq->front = (dq->front + 1) % Size;
         }
+        cout << x << " is successfully Deleted\n";
+    }
+}
+void deletion_from_end(deque *dq) {
+    int x;
+    if (dq->front == -1)
+        cout << "Queue is Empty\n";
+    else {
+        x = dq->arr[dq->rear];
+        if (dq->front == dq->rear) {
+            dq->front = -1;
+            dq->rear = -1;
+        } else if (dq->rear == 0)
+            dq->rear = Size - 1;
+        else
+            dq->rear--;
+        cout << x << " is successfully Deleted\n";
     }
 }
 
-void display(deQue *dq) {
-    if (dq->front == -1) {
-        cout << "Deque is empty\n";
-    } else {
-        if (dq->front <= dq->rare) {
-            for (int i = dq->front; i <= dq->rare; i++) {
+void display(deque *dq) {
+    if (dq->front == -1)
+        cout << "List is Empty\n";
+    else {
+        if (dq->front <= dq->rear) {
+            for (int i = dq->front; i <= dq->rear; i++) {
                 cout << dq->arr[i] << " ";
             }
+            cout << "\n";
         } else {
-            for (int i = dq->front; i < size; i++) cout << dq->arr[i] << " ";
-            for (int i = 0; i <= dq->rare; i++) cout << dq->arr[i] << " ";
+            for (int i = dq->front; i < Size; i++) {
+                cout << dq->arr[i] << " ";
+            }
+            for (int i = 0; i <= dq->rear; i++) {
+                cout << dq->arr[i] << " ";
+            }
+            cout << "\n";
         }
-        cout << endl;
     }
 }
 
 int main() {
-    deQue dq;
-    dq.rare = -1;
-    dq.front = -1;
-
+    deque dq;
+    dq.front = -1, dq.rear = -1;
     int ch;
     while (1) {
-        cout << "1. insert front\n";
-        cout << "2. insert rare\n";
-        cout << "3. delete front\n";
-        cout << "4. delete rare\n";
-        cout << "5. disaply\n";
-        cout << "6. exit\n";
-        cout << "Enter : ";
+        cout << "\n1. Insertion at rear";
+        cout << "\n2. Insertion at front";
+        cout << "\n3. Deletion from front";
+        cout << "\n4. Deletion from end";
+        cout << "\n5. Display";
+        cout << "\n6. Exit ";
+        cout << "\n Enter your Choice: ";
         cin >> ch;
         switch (ch) {
             case 1:
-                insert_front_end(&dq);
+                insertion_at_rear(&dq);
                 break;
+
             case 2:
-                insert_rare_end(&dq);
+                insertion_at_front(&dq);
                 break;
+
             case 3:
-                delete_front_end(&dq);
+                deletion_from_front(&dq);
                 break;
+
             case 4:
-                delete_rare_end(&dq);
+                deletion_from_end(&dq);
                 break;
+
             case 5:
                 display(&dq);
                 break;
+
             case 6:
+                cout << "EXIT SUCCESSFULLY\n";
                 exit(0);
 
             default:
+                cout << "Enter the correct choice\n";
                 break;
         }
     }
